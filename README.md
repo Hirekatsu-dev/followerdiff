f10a61efa20d8b50fbe9898cbe9da4033ef695cd
Hirekatsu-dev <hirekatsu535@icloud.com>
2022-05-08 20:52:02 +0900

    add: API

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 ```sh
 docker-compose exec gas clasp login --no-localhost
 ```
+
+
+## APIを叩く
+
+`https://script.google.com/macros/s/AKfycbzBuMVhHPgxHyTCH5NLDGfDxtTXmZFRdLDdje8PEZ1VX-6cLWGrAPoXMBfF-7ET8nNP/exec/followers/diff?id=1000&from=2022-05-05&to=2022-05-05`
+
+みたいな感じ
--- apps/gas/app/src/main.ts
+++ apps/gas/app/src/main.ts
@@ -7,6 +7,35 @@ const TWITTER_ACCESS_TOKEN = PropertiesService.getScriptProperties().getProperty
 const TWITTER_ACCESS_SECRET_TOKEN = PropertiesService.getScriptProperties().getProperty('TWITTER_ACCESS_SECRET_TOKEN') || '';
 const SHEET_ID = PropertiesService.getScriptProperties().getProperty('SHEET_ID') || '';
 
+const logSheet = SpreadsheetApp.openById(SHEET_ID).getSheetByName('logs');
+const logMaxRow = 1000;
+
+// ログ
+function log(level: 'info' | 'warn' | 'error', message: string) {
+	switch(level) {
+		case 'info':
+			console.log(message);
+			break;
+		case 'warn':
+			console.warn(message);
+			break;
+		case 'error':
+			console.error(message);
+			break;
+	}
+
+	if (!logSheet) {
+		console.error('logSheet not found.');
+		return;
+	}
+
+	logSheet.appendRow([new Date(), level.toUpperCase(), message])
+
+	if (logMaxRow < logSheet.getLastRow()) {
+		logSheet.deleteRow(2)
+	}
+}
+
 // モデル
 class Follower {
 	constructor(
@@ -94,7 +123,7 @@ class TwitterClient {
 
 				// 余裕を持って1秒ほど余分に待つ
 				const waitTimeMilliSeconds = Number(rateLimitResetAt) * 1000 - Date.now() + 1000;
-				console.log(`faced to rate limit. wait for ${ waitTimeMilliSeconds } ms.`);
+				log('error', `faced to rate limit. wait for ${ waitTimeMilliSeconds } ms.`);
 				Utilities.sleep(waitTimeMilliSeconds);
 				continue;
 			} else if (response.getResponseCode() !== 200) {
@@ -140,15 +169,15 @@ async function fetchFollowers() {
 		if (matched) {
 			const id = matched[1];
 
-			console.log(`begin fetch followers of ID ${id}`);
+			log('info', `begin fetch followers of ID ${id}`);
 
 			try {
 				const followers = fetchFollowersById(twitterClient, id, date);
 				writeFollowers(id, followers);
 
-				console.log(`finish fetch followers of ID ${id}`);
+				log('info', `finish fetch followers of ID ${id}`);
 			} catch (e) {
-				console.log(`failed to fetch followers of ID ${id}: ${e}`);
+				log('error', `failed to fetch followers of ID ${id}: ${e}`);
 			}
 		}
 	});
@@ -179,4 +208,161 @@ function writeFollowers(id: string, followers: Follower[]) {
 		const lastRow = sheet.getLastRow();
 		sheet.getRange(lastRow + 1, 1, appendRows.length, appendRows[0].length).setValues(appendRows);
 	}
+}
+
+
+
+//// API ////
+
+type GetFollowerDiffItem = {
+	twitterId: string,
+	diff: '+' | '-'
+	screenName: string,
+	name: string,
+}
+
+function getFollowerDiff(id: string, from: string, to: string): GetFollowerDiffItem[] {
+	// query 関数に結果を格納するためのシートを作る
+	const sheet = SpreadsheetApp.openById(SHEET_ID).getSheetByName(`followers_${id}`);
+
+	if (!sheet) {
+		throw Error(`sheet followers_${id} not found`);
+	}
+
+	const fromFollowers = getFollowersAt(sheet, from);
+	const toFollowers = getFollowersAt(sheet, to);
+
+	const fromFollowerIds = new Set(fromFollowers.map(follower => follower.twitterId));
+	const toFollowerIds = new Set(toFollowers.map(follower => follower.twitterId));
+
+	const result: GetFollowerDiffItem[] = [];
+
+	toFollowers.forEach(follower => {
+		if(!fromFollowerIds.has(follower.twitterId)) {
+			result.push({
+				twitterId: follower.twitterId,
+				diff: '+',
+				screenName: follower.screenName,
+				name: follower.name
+			})
+		}
+	});
+
+	fromFollowers.forEach(follower => {
+		if(!toFollowerIds.has(follower.twitterId)) {
+			result.push({
+				twitterId: follower.twitterId,
+				diff: '-',
+				screenName: follower.screenName,
+				name: follower.name
+			})
+		}
+	});
+
+	return result;
+}
+
+function getFollowersAt(sheet: GoogleAppsScript.Spreadsheet.Sheet, date: string): Follower[] {
+	return [];
+}
+
+
+type getFollowerDiffRequestParams = {
+	id: string,
+	from: string,
+	to: string
+}
+
+function validateGetFollowerDiffParams(params: Record<string, string>): params is getFollowerDiffRequestParams {
+	if (!params['id']) {
+		throw Error('id is required.');
+	} else {
+		if (!params['id'].match(/^\d+$/)) {
+			throw Error('id is invalid.');
+		}
+	}
+
+	if (!params['from']) {
+		throw Error('from is required.');
+	} else {
+		if (!params['from'].match(/^\d{4}-\d{2}-\d{2}$/)) {
+			throw Error('from is invalid.');
+		}
+	}
+
+	if (!params['to']) {
+		throw Error('to is required.');
+	} else {
+		if (!params['to'].match(/^\d{4}-\d{2}-\d{2}$/)) {
+			throw Error('from is invalid.');
+		}
+	}
+
+	return true;
+}
+
+function execGetFollowerDiffApi(params: Record<string, string>) {
+	try {
+		if (validateGetFollowerDiffParams(params)) {
+			const followers = getFollowerDiff(params['id'], params['from'], params['to']);
+
+
+			return ({
+				status: 200,
+				resultCode: '00101',
+				list: followers
+			})
+		}
+
+		throw Error('unknown error');
+	} catch(e) {
+		if (e instanceof Error) {
+			return ({
+				status: 500,
+				message: e.message
+			})
+		}
+	}
+}
+
+type Request = {
+	queryString: string | null,
+	pathInfo: string,
+	parameter: Record<string, any>,
+	parameters: Record<string, any>,
+}
+
+function doGet(request: Request) {
+	const jobUuid = Utilities.getUuid();
+	const path = request.pathInfo;
+
+	log('info', `[${ jobUuid }] GET ${ path }: begin.`);
+
+	let result = null;
+
+	switch(path) {
+		case 'followers/diff': {
+			result = execGetFollowerDiffApi(request.parameter);
+			break;
+		}
+		default: {
+			result = ({
+				status: 404
+			});
+			break;
+		}
+	}
+
+	if (result?.status) {
+		if (result.status === 200) {
+			log('info', `[${ jobUuid }] GET ${ path }: finished with status 200.`);
+		} else if (result.status < 500) {
+			log('info', `[${ jobUuid }] GET ${ path }: finished with status ${ result.status }.`);
+		} else {
+			log('error', `[${ jobUuid }] GET ${ path }: finished with status ${ result.status }. response: ${ result }`);
+		}
+	}
+
+	return ContentService.createTextOutput(JSON.stringify(result))
+		.setMimeType(ContentService.MimeType.JSON);
 }
